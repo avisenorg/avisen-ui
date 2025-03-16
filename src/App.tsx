5036858e78bfabacab7765493fdba60924187ac7
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2025-03-16 11:16:39 -0500

    See latest blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import logo from './assets/avisen.svg'
 import './App.css'
+import {useEffect, useState} from "react";
+
+interface Block {
+  publisherKey: string;
+  hash: string;
+  previousHash: string;
+  timestamp: string;
+  height: number;
+}
+
+async function fetchLatestBlocks(): Promise<Block[]> {
+  const response = await fetch('http://localhost:8081/blockchain?page=0&size=10&sort=DESC', {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Network-ID": "local",
+    }
+  });
+  return await response.json();
+}
 
 function App() {
+
+  const [latestBlocks, setLatestBlocks] = useState<Block[]>([]);
+  useEffect(() => {
+    fetchLatestBlocks().then((blocks) => setLatestBlocks(blocks))
+  })
 
   return (
     <>
       <div>
-        <a href="https://github.com/avisenorg" target="_blank">
-          <img src={logo} className="logo" alt="Avisen logo" />
-        </a>
+          <h2>Latest Blocks</h2>
+          <ol>
+            {
+              latestBlocks.map((block) => (
+                <li id={block.hash}>
+                  <a>{block.hash}</a>
+                </li>
+              ))
+            }
+          </ol>
       </div>
-      <h1>Avisen</h1>
     </>
   )
 }
